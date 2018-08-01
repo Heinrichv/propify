242ea7965bab9c54516d33163ada310ef059e81e
Heinrich Venter <heini141@outlook.com>
2018-08-01 21:58:17 +0200

    feat(nav, bootstrap): added bootstrap css and nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LoggedoutComponent } from './loggedout/loggedout.component';
+import { LoggedinComponent } from './loggedin/loggedin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoggedoutComponent,
+    LoggedinComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot([
+      { path: 'home', component: AppComponent },
+      { path: 'how-it-works', component: AppComponent },
+      { path: 'contact-us', component: AppComponent },
+      { path: 'sign-up', component: AppComponent },
+      { path: '*', component: AppComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
